b0ed4170564aa659df856b2c2bfe3e5979d943b9
Hacker Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-02-14 01:28:22 +0630

    Enhance authentication response to include JWT and restore privilege checks

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,7 +22,10 @@ const authenticateUser = async (email, password, res) => {
         const token = jwt.sign({ _id: user.id, dept_name: user.dept_name }, process.env.JWT_SECRET, { expiresIn: '8h' });
 
         res.cookie('jwt', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production', maxAge: timer, sameSite: 'Strict'  });
-        return res.json({ message: 'Login successful' });
+        return res.json({ 
+            message: 'Login successful',
+            jwt : token
+         });
     } catch (error) {
         console.error("Error during authentication:", error);
         if (!res.headersSent) {
