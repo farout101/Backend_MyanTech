61eaefba8b79fc3453af05b7c4d09855b7b7cc9a
Myatty <myintmyatps664@gmail.com>
2025-02-13 11:19:27 +0630

    change driver route

--- src/routes/drivers.js
+++ src/routes/drivers.js
@@ -12,7 +12,7 @@ const {
 const router = express.Router();
 
 router.get("/search", getDriverByName); // GET /api/drivers/search?driver_name=JohnDoe
-//router.get("/all", getAllDrivers); // GET /api/drivers?limit=100&offset=0
+router.get("/all", getAllDrivers); // GET /api/drivers?limit=100&offset=0
 router.post("/", createDriver); // POST /api/drivers
 router.put("/", updateDriver); // PUT /api/drivers
 router.delete("/", deleteDriver); // DELETE /api/drivers
